2a9d2b30772cedb309429e49577b3f2175a67d21
John Barrett <johnjbarrett22@gmail.com>
2019-04-07 04:15:31 -0700

    Migrations made, corrected duplicate line in models.py and fleshed out a creation form for easy DB entry

--- apps/pagination_app/views.py
+++ apps/pagination_app/views.py
@@ -5,3 +5,7 @@ def index(request):
 
 def add(request):
     return render(request, "pagination_app/add.html")
+
+def create(request):
+    User.objects.create(first_name=request.POST['first_name'], last_name=request.POST['last_name'], email_address=request.POST['email_address'])
+    return redirect(request, "pagination_app/index.html")
