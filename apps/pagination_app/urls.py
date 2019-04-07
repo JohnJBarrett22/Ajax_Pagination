88b6c58ce63ae2922623642fe9ca933f8296e3a3
John Barrett <johnjbarrett22@gmail.com>
2019-04-07 05:48:13 -0700

    Finished the find function, added additional .html file and am working on date sort

--- apps/pagination_app/urls.py
+++ apps/pagination_app/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('add', views.add)
+    path('add', views.add),
+    path('create', views.create),
+    path('find', views.find)
 ]
--- apps/pagination_app/views.py
+++ apps/pagination_app/views.py
@@ -1,11 +1,33 @@
 from django.shortcuts import render, redirect, HttpResponse
+from . models import *
 
 def index(request):
-    return render(request, "pagination_app/index.html")
+    users = User.objects.all()
+    return render(request, "pagination_app/index.html", {"users": users})
 
 def add(request):
     return render(request, "pagination_app/add.html")
 
 def create(request):
     User.objects.create(first_name=request.POST['first_name'], last_name=request.POST['last_name'], email_address=request.POST['email_address'])
-    return redirect(request, "pagination_app/index.html")
+    return redirect('/')
+
+def find(request):
+    users = User.objects.filter(first_name__startswith=request.POST['lead_name'])|User.objects.filter(last_name__startswith=request.POST['lead_name'])
+    print(users)
+    return render(request, "pagination_app/table.html", {"users": users})
+
+def date(request):
+    if len(request.POST['from_date']) < 1:
+        from_date = '0000-01-01'
+    else:
+        from_date = request.POST['from_date']
+
+    if len(request.POST['to_date']) < 1:
+        from_date = '9999-12-31'
+    else:
+        from_date = request.POST['to_date']
+
+    users = User.objects.filter(date__range=[from_date, to_date])
+    print(users)
+    return render(request, "pagination_app/table.html", {"users": users})
